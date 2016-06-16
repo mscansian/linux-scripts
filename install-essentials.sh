9cdf1dc4b51633289793193d5dc865616f16c88d
Matheus Cansian <mscansian@gmail.com>
2016-06-16 13:53:59 -0300

    Added git config

--- install-essentials.sh
+++ install-essentials.sh
@@ -19,7 +19,7 @@ sudo sh -c "cat /usr/share/software-center/ui/gtk3/css/softwarecenter.css | sed 
 #Every install is on a different line so you can comment out things that you don't want
 sudo apt-get install ubuntu-restricted-extras -y
 sudo apt-get install git gitk -y
-sudo apt-get install python-pip -y
+sudo apt-get install python-pip python3-pip -y
 sudo apt-get install gufw -y
 sudo apt-get install filezilla -y
 sudo apt-get install gimp -y
@@ -32,18 +32,28 @@ sudo apt-get install gedit-plugins -y
 sudo apt-get install samba -y
 sudo apt-get install keepass2 -y
 sudo apt-get install wine -y
-sudo apt-get install make -y
-sudo apt-get install gnome-shell-pomodoro -y
+sudo apt-get install gpick -y
+sudo apt-get install htop -y
+sudo apt-get install make cmake cmake-curses-gui -y
+#sudo apt-get install gnome-shell-pomodoro -y
+sudo apt-get install python-dev python3-dev -y
 #sudo apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-sqlite php5-curl -y
+
+sudo pip install virtualenv -y
+
+# Set up GIT
+git config --global user.email "mscansian@gmail.com"
+git config --global user.name "Matheus Cansian"
 
 chmod +x *.sh
 chmod +x app-install/*.sh
 
 #f.luxgui
-cd $curdir && source app-install/fluxgui.sh
+#cd $curdir && source app-install/fluxgui.sh
 
 #Calibre
 cd && sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 
 cd $curdir && source app-install/steam.sh
 cd $curdir && source app-install/chrome.sh
+cd $curdir && source app-install/spotify.sh
--- templates/.bash_aliases
+++ templates/.bash_aliases
@@ -1,3 +1,35 @@
 alias myip='wget -qO - http://drpexe.com/ip.php && echo'
 alias rm-pyc='find . -name "*.pyc" -type f -delete'
 alias rm-tmp='find . -name "~*" -type f -delete'
+
+# Python validators
+alias pep8-100='pep8 --max-line-length=99'
+alias pylint-100='pylint --max-line-length=100'
+
+# Git stuff
+alias gs='git status'
+alias gc='git commit'
+alias gla='git logall'
+alias glb='git log --oneline --decorate'
+alias ga='git add'
+alias gck='git checkout'
+alias gp='git push'
+alias gdc='git diff --cached'
+alias gap='git add --patch'
+alias gd='git diff'
+alias gf='git fetch'
+alias gsl='git stash list'
+alias gss='git stash save'
+alias gsp='git stash pop'
+
+cpulimit-c() {
+    cpulimit -l $1 -- $2
+}
+
+delay-network() {
+    sudo tc qdisc add dev lo root netem delay $1
+}
+
+delay-network-clear() {
+    sudo tc qdisc del dev lo root
+}
