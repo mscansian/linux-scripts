f06976b663c911a29af38f3697d043137b986222
Matheus Cansian <mscansian@gmail.com>
2017-05-04 12:08:53 -0300

    updated docker

--- app-install/docker.sh
+++ app-install/docker.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install apt-transport-https ca-certificates curl
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
 sudo apt-get update
-sudo apt-get purge lxc-docker
 
 sudo apt-get install linux-image-extra-$(uname -r)
-sudo apt-get install docker-engine
+sudo apt-get -y install docker-ce
 
 sudo service docker start
 sudo groupadd docker
